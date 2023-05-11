92e50233ee834478eddf5d5c2e0b7ede42513848
Drew Bidmead <dmbidmead@gmail.com>
2023-05-11 13:42:55 -0400

    Initial styling for desktop complete with error styled in CSS and error messages stored in JS constants

---
+++ app.js
@@ -0,0 +1,6 @@
+
+
+const emptyMessage = 'This field is required';
+const dayMessage = 'Must be a valid day';
+const monthMessage = 'Must be a valid month';
+const yearMessage = 'Must be in the past';
