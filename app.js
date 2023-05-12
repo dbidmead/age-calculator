0ef8365e03fc76bf8e4278e8d440c9675df5f894
Drew Bidmead <dmbidmead@gmail.com>
2023-05-12 00:47:34 -0400

    CSS styling based on validation logic implemented

--- app.js
+++ app.js
@@ -17,9 +17,11 @@ const TODAY = {
     year: today.getFullYear()
 };
 
+
+
 // --------------------------VALIDATION LOGIC-----------------------------------
 
-// main form validator
+// -----main form validator-----
 const isFormValid = (day, month, year) => {
     if(
         day.error == 'none' && 
@@ -29,12 +31,40 @@ const isFormValid = (day, month, year) => {
     else return false;
 }
 
-// main input validator
+
+// -----main css validation handler-----
+function styleInput(input, message) {
+    if(input.error == 'none') {
+        input.parentElement.classList.remove('error');
+    } else if(input.error != 'none') {
+        input.parentElement.classList.add('error');
+    };
+    styleError(input, message);
+}
+
+function styleError(inputObj, message) {
+    let errorEl = inputObj.parentElement.lastElementChild;
+    if(inputObj.error == 'empty') {
+        errorEl.setAttribute('style', 'opacity: 1');
+        errorEl.textContent = emptyMessage;
+    } else if(inputObj.error == 'invalid'){
+        errorEl.setAttribute('style', 'opacity: 1');
+        errorEl.textContent = message;
+    } else {
+        errorEl.setAttribute('style', 'opacity: 0');
+        errorEl.textContent = '';
+    }
+}
+
+
+// -----generic input validator-----
 const numRegEx = /^[0-9]+$/;
-const isInputValid = (inputObj, digitQty, upperBound) => {
+const isInputValid = (inputObj, digitQty, upperBound, message) => {
     inputObj.error = 'none';
+    inputObj.parentElement.classList.remove('error');
     if(inputObj.value.length == 0) {
         inputObj.error = 'empty';
+        styleInput(inputObj, message);
         return false;
 
     } else if(
@@ -44,10 +74,16 @@ const isInputValid = (inputObj, digitQty, upperBound) => {
         inputObj.value.length != digitQty
         ) {
             inputObj.error = 'invalid';
+            styleInput(inputObj, message);
             return false;
 
-    } else return true;
+    } else {
+        inputObj.error = 'none';
+        styleInput(inputObj, message);
+        return true;
+    } 
 }
+
 
 // -----is day valid?-----
 function validateDay() {
@@ -58,15 +94,15 @@ function validateDay() {
         } else {
             dayUpperBound = setDayUpperBound(MONTH.num);
         }
-    }
+    } 
     DAY.num = parseInt(DAY.value);
-    return isInputValid(DAY, 2, dayUpperBound);
+    return isInputValid(DAY, 2, dayUpperBound, dayMessage);
     // console.log(`day error: ${DAY.error}, upper day limit: ${dayUpperBound}`)
 };
 
 // set day upper bound
 const setDayUpperBound = (monthInputValue, yearInputValue) => {
-    if(yearInputValue == TODAY.year && monthInputValue == TODAY.month) {
+    if(yearInputValue == TODAY.year && monthInputValue >= TODAY.month) {
         return TODAY.day;
     } else {
         let monthIndex = monthInputValue - 1;
@@ -104,6 +140,7 @@ const checkLeapYear = (year) => {
     } else return false;
 }
 
+
 // -----is month valid?-----
 function validateMonth() {
     let monthUpperBound = 12;
@@ -111,7 +148,7 @@ function validateMonth() {
         monthUpperBound = setMonthUpperBound(YEAR.num);
     }
     MONTH.num = parseInt(MONTH.value);
-    return isInputValid(MONTH, 2, monthUpperBound);
+    return isInputValid(MONTH, 2, monthUpperBound, monthMessage);
     // console.log(`month error: ${MONTH.error}, upper month limit: ${monthUpperBound}`)
 };
 
@@ -122,24 +159,34 @@ const setMonthUpperBound = (yearInputValue) => {
     } else return 12;
 }
 
+
 // -----is year valid?-----
 function validateYear() {
     YEAR.num = parseInt(YEAR.value);
-    return isInputValid(YEAR, 4, TODAY.year);
+    return isInputValid(YEAR, 4, TODAY.year, yearMessage);
     // console.log(`year error: ${YEAR.error}`)
 }
 
+
 // -----validation event handlers-----
-function handleInput() {
-    validateDay();
+function handleInput(e) {
+    if(!validateYear()) {
+        if(YEAR.num >= TODAY.year) {
+            DAY.parentElement.classList.add('error');
+            MONTH.parentElement.classList.add('error');
+            return;
+        };  
+    };
     validateMonth();
-    validateYear();
-    console.log(validateDay(), validateMonth(), validateYear())
+    validateDay();
+    // console.log(validateDay(), validateMonth(), validateYear(), e.target.parentElement)
 };
 
 DAY.addEventListener('input', handleInput)
 MONTH.addEventListener('input', handleInput)
 YEAR.addEventListener('input', handleInput)
+
+
 
 // --------------------------SUBMISSION LOGIC-----------------------------------
 
@@ -157,5 +204,14 @@ YEAR.addEventListener('input', handleInput)
 SUBMIT.addEventListener('click', (e) => {
     if(!isFormValid(DAY, MONTH, YEAR)) {
         e.preventDefault();
+        if (!DAY.parentElement.classList.contains('error')) {
+            DAY.parentElement.classList.add('error');
+        };
+        if (!MONTH.parentElement.classList.contains('error')) {
+            MONTH.parentElement.classList.add('error');
+        };
+        if (!YEAR.parentElement.classList.contains('error')) {
+            YEAR.parentElement.classList.add('error');
+        };
     } else return;
 })
